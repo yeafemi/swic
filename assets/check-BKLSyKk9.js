a43a025deea522e3630abf265e7d80e1b5c80815
yeafemi <yea.femi@gmail.com>
2026-05-26 17:42:59 +0000

    Deploying to gh-pages from @ yeafemi/swic@9b45b5c9288df3d446a63ef4663f9cdc3996efc6 🚀

---
+++ assets/check-BKLSyKk9.js
@@ -0,0 +1,1 @@
+import{q as c}from"./index-DfXuRNoL.js";const e=[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]],t=c("check",e);export{t as C};
--- assets/check-DQAntYAw.js
+++
@@ -1,1 +0,0 @@
-import{q as c}from"./index-Bs-CX7_Q.js";const e=[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]],t=c("check",e);export{t as C};
